ba0297c90d8dd0a8317ad740e915d0fdcc0644e4
David Mouton <damo@tamina-online.com>
2014-05-26 16:51:56 +0200

    + haxe2 symlink

--- bin/haxe-cli.js
+++ bin/haxe-cli.js
@@ -1,2 +1,32 @@
 #!/usr/bin/env node
-console.log('yeahhhh');
+var os = require('os');
+var fs = require('fs');
+var path = require('path');
+var exec = require('child_process').exec;
+
+var platform = os.platform();
+var haxeExec = 'haxe';
+if(platform == 'win32' || platform == 'win64'){
+	haxeExec = 'haxe.exe'
+}
+var cmd = path.dirname(fs.realpathSync(__filename)) + '/' + haxeExec;
+process.argv.forEach(function (val, index, array) {
+	if(index >1){
+	cmd += ' ' + val
+  }
+});
+console.log( '--> ' + cmd);
+exec(cmd,function(error, stdout, stderr){
+	if (error !== null) 
+	{
+	  console.error(error);
+	} 
+	else if(stderr != null)
+	{
+		console.log(stderr);
+	}
+	else
+	{
+		console.log(stdout);
+	}
+});
--- bin/install.js
+++ bin/install.js
@@ -1,5 +1,5 @@
 var os = require('os');
-var exec = require('child_process').exec
+var exec = require('child_process').exec;
 var fs = require('fs');
 var zlib = require('zlib');
 
@@ -7,24 +7,19 @@ var platform = os.platform();
 console.log("Installing Binaries for  " + os.platform() );
 var inputFile='';
 var outputFile='';
-var alias_cmd='';
 switch(platform) {
 	case 'linux':
 		inputFile = 'haxe-linux32.gz';
 		outputFile='haxe';
-		alias_cmd = "alias haxe='"+__dirname+"/haxe'";
 		break;
 	case 'darwin':
 		inputFile = 'haxe-osx.gz';
 		outputFile='haxe';
-		alias_cmd = "alias haxe='"+__dirname+"/haxe'";
 		break;	
 	case 'win32':
 	case 'win64':
 		inputFile = 'haxe-win.gz';
 		outputFile='haxe.exe';
-		alias_cmd = 'setx PATH "%PATH%;'+__dirname+'"';
-		
 		break;
 	default:
 		console.error("ERROR : Unknown plateform");
@@ -38,9 +33,3 @@ zlib.gunzip(input,function(error,result){
 		fs.writeFileSync('./bin/'+outputFile,result);
 	}
 });
-console.log(alias_cmd);
-/*exec(alias_cmd,function(error, stdout, stderr){
-	if (error !== null) {
-	  console.error(error);
-	}
-});*/
