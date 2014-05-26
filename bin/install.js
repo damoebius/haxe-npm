1f1da8524cd572ad2cfd7374d5f4d826b2f88ddb
David Mouton <damo@tamina-online.com>
2014-05-26 12:35:37 +0200

    + bin

--- bin/install.js
+++ bin/install.js
@@ -1,1 +1,46 @@
-console.log("Installing Binaries");
+var os = require('os');
+var exec = require('child_process').exec
+var fs = require('fs');
+var zlib = require('zlib');
+
+var platform = os.platform();
+console.log("Installing Binaries for  " + os.platform() );
+var inputFile='';
+var outputFile='';
+var alias_cmd='';
+switch(platform) {
+	case 'linux':
+		inputFile = 'haxe-linux32.gz';
+		outputFile='haxe';
+		alias_cmd = "alias haxe='"+__dirname+"/haxe'";
+		break;
+	case 'darwin':
+		inputFile = 'haxe-osx.gz';
+		outputFile='haxe';
+		alias_cmd = "alias haxe='"+__dirname+"/haxe'";
+		break;	
+	case 'win32':
+	case 'win64':
+		inputFile = 'haxe-win.gz';
+		outputFile='haxe.exe';
+		alias_cmd = 'setx PATH "%PATH%;'+__dirname+'"';
+		
+		break;
+	default:
+		console.error("ERROR : Unknown plateform");
+}
+
+var input = fs.readFileSync("./bin/"+inputFile);
+zlib.gunzip(input,function(error,result){
+	if(error != null){
+		console.error(error);
+	} else {
+		fs.writeFileSync('./bin/'+outputFile,result);
+	}
+});
+console.log(alias_cmd);
+exec(alias_cmd,function(error, stdout, stderr){
+	if (error !== null) {
+	  console.error(error);
+	}
+});
