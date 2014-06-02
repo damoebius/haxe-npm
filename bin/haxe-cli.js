80b7bb7d8de83e983df8d9c9f5495f3e8281e78b
David Mouton <damo@tamina-online.com>
2014-06-02 17:21:35 +0200

    + use and lib arg support

--- bin/haxe-cli.js
+++ bin/haxe-cli.js
@@ -6,28 +6,32 @@ var exec = require('child_process').exec;
 
 var platform = os.platform();
 var haxeExec = 'haxe';
-if(platform == 'win32' || platform == 'win64'){
-	haxeExec = 'haxe.exe'
+if (platform == 'win32' || platform == 'win64') {
+    haxeExec = 'haxe.exe'
 }
-var cmd = path.dirname(fs.realpathSync(__filename)) + '/' + haxeExec;
+var haxeDir = path.dirname(fs.realpathSync(__filename));
+var cmd = haxeDir + '/' + haxeExec + ' ';
 
 process.argv.forEach(function (val, index, array) {
-	if(index >1){
-	cmd += ' ' + val
-  }
+    if (index > 1) {
+        if (val == '-lib') {
+            cmd += '-cp ' + haxeDir + '/../../';
+        } else if(val == '-use'){
+            cmd += '-cp ' + haxeDir + '/../lib/';
+        }
+        else {
+            cmd += val + ' ';
+        }
+    }
 });
-console.log( '--> ' + cmd);
-exec(cmd,function(error, stdout, stderr){
-	if (error !== null) 
-	{
-	  console.error(error);
-	} 
-	else if(stderr != null)
-	{
-		console.log(stderr);
-	}
-	else
-	{
-		console.log(stdout);
-	}
+exec(cmd, function (error, stdout, stderr) {
+    if (error !== null) {
+        console.error(error);
+    }
+    else if (stderr != null) {
+        console.log(stderr);
+    }
+    else {
+        console.log(stdout);
+    }
 });
