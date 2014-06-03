e981d0de3b7a168305e7422a1d6bd428ae775a0a
David Mouton <damo@tamina-online.com>
2014-06-03 09:58:46 +0200

    + use and lib arg support

--- bin/haxe-cli.js
+++ bin/haxe-cli.js
@@ -5,12 +5,14 @@ var path = require('path');
 var exec = require('child_process').exec;
 
 var platform = os.platform();
-var haxeExec = 'haxe';
-if (platform == 'win32' || platform == 'win64') {
-    haxeExec = 'haxe.exe'
+var haxeExec = 'haxe.bin';
+var stdPath = path.dirname(fs.realpathSync(__filename)) + '/../std';
+var cmd = '';
+if(platform == 'win32' || platform == 'win64'){
+    haxeExec = 'haxe.exe' ;
 }
 var haxeDir = path.dirname(fs.realpathSync(__filename));
-var cmd = haxeDir + '/' + haxeExec + ' ';
+cmd += haxeDir + '/' + haxeExec + ' ';
 
 process.argv.forEach(function (val, index, array) {
     if (index > 1) {
@@ -24,14 +26,17 @@ process.argv.forEach(function (val, index, array) {
         }
     }
 });
-exec(cmd, function (error, stdout, stderr) {
-    if (error !== null) {
+exec(cmd,{env: {'HAXE_STD_PATH': stdPath}},function(error, stdout, stderr){
+    if (error !== null)
+    {
         console.error(error);
     }
-    else if (stderr != null) {
+    else if(stderr != null)
+    {
         console.log(stderr);
     }
-    else {
+    else
+    {
         console.log(stdout);
     }
 });
--- bin/install.js
+++ bin/install.js
@@ -11,36 +11,63 @@ var alias_cmd='';
 var stdPath=__dirname+'/../std';
 
 switch(platform) {
-	case 'linux':
-		inputFile = 'haxe-linux32.gz';
-		outputFile='haxe';
-		alias_cmd = 'grep "HAXE_STD_PATH" /etc/environment || echo "export HAXE_STD_PATH='+stdPath+':." | sudo tee -a /etc/environment';
-		break;
-	case 'darwin':
-		inputFile = 'haxe-osx.gz';
-		outputFile='haxe';
-		alias_cmd = 'grep "HAXE_STD_PATH" /etc/environment || echo "export HAXE_STD_PATH='+stdPath+':." | sudo tee -a /etc/environment';
-		break;	
-	case 'win32':
-	case 'win64':
-		inputFile = 'haxe-win.gz';
-		outputFile='haxe.exe';
-		alias_cmd = 'setx HAXE_STD_PATH "'+stdPath+'"';
-		break;
-	default:
-		console.error("ERROR : Unknown plateform");
-}
+    case 'linux':
+        inputFile = 'haxe-linux64.gz';
+        outputFile = 'haxe.bin';
+        unzip(inputFile, function ( error, result ) {
+            if ( error != null ) {
+                console.error(error);
+            } else {
+                fs.writeFileSync('./bin/' + outputFile, result);
+                exec('chmod 755 ' + __dirname + '/' + outputFile, function ( error, stdout, stderr ) {
+                    if ( error !== null ) {
+                        console.error(error);
+                    }
+                });
+            }
+        });
+        break;
+    case 'darwin':
+        inputFile = 'haxe-osx.gz';
+        outputFile = 'haxe.bin';
+        unzip(inputFile, function ( error, result ) {
+            if ( error != null ) {
+                console.error(error);
+            } else {
+                fs.writeFileSync('./bin/' + outputFile, result);
+                exec('chmod 755 ' + __dirname + '/' + outputFile, function ( error, stdout, stderr ) {
+                    if ( error !== null ) {
+                        console.error(error);
+                    }
+                });
+            }
+        });
 
-var input = fs.readFileSync("./bin/"+inputFile);
-zlib.gunzip(input,function(error,result){
-	if(error != null){
-		console.error(error);
-	} else {
-		fs.writeFileSync('./bin/'+outputFile,result);
-	}
-});
-exec(alias_cmd,function(error, stdout, stderr){
-	if (error !== null) {
-	  console.error(error);
-	}
-});
+        break;
+    case 'win32':
+    case 'win64':
+        inputFile = 'haxe-win.gz';
+        outputFile = 'haxe.exe';
+        alias_cmd = 'setx HAXE_STD_PATH "' + stdPath + '"';
+        unzip(inputFile, function ( error, result ) {
+            if ( error != null ) {
+                console.error(error);
+            } else {
+                fs.writeFileSync('./bin/' + outputFile, result);
+            }
+        });
+        exec(alias_cmd, function ( error, stdout, stderr ) {
+            if ( error !== null ) {
+                console.error(error);
+            }
+
+
+        });
+        break;
+    default:
+        console.error("ERROR : Unknown plateform");
+}
+function unzip(inputFile, callback){
+    var input = fs.readFileSync("./bin/"+inputFile);
+    zlib.gunzip(input,callback);
+}
